d7893b75d22c48d479e29e90e219c19a9453fb5e
Ren <warren.pusing.equinan@gmail.com>
2025-10-04 20:11:18 +0800

    Add toggle feature in nav on mobile

--- src/layout/HeaderComponent.jsx
+++ src/layout/HeaderComponent.jsx
@@ -1,14 +1,31 @@
 import myLogo from "../assets/logos/renLogo.svg";
 import Button from "../components/Button";
+import { useState } from "react";
 
 function HeaderComponent() {
+
+  const [toggle, setToggle] = useState(false);
+
+  function toggleClass() {
+     setToggle(() => !toggle);
+  }
+
+  //nav-menu-icon d-lg-none
   return (
     <header>
       <nav className="container-fluid nav">
         <div className="container">
             <div className="nav-inner">
+
                 <img src={ myLogo } alt="My Logo" className="nav-logo" />
-                <ul className="nav-list">
+                
+                <div className={`nav-menu-icon d-lg-none ${toggle ? "toggle" : ""}`} onClick={toggleClass}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <ul className={`nav-list ${toggle ? "toggle" : ""}`}>
                     <li className="nav-item">
                       <a className="nav-link" href="#">Home</a>
                     </li>
@@ -21,12 +38,13 @@ function HeaderComponent() {
                     <li className="nav-item">
                       <a className="nav-link" href="#">Contact</a>
                     </li>
-                     <li className="nav-item d-lg-none d-md-block ">
+                     <li className="nav-item d-lg-none d-flex">
                         <Button href="#" btnText="Download CV" className="nav-btn-2" />
                     </li>
                 </ul>
+
             </div>
-          <Button href="#" btnText="Download CV" className="nav-btn d-none d-lg-block" />
+          <Button href="#" btnText="Download CV" className="nav-btn d-none d-lg-flex" />
         </div>
       </nav>
     </header>
