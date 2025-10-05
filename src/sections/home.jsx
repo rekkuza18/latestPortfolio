5789ec9e88faff7ea3fc4a4fb3278e00b92a65af
Ren <warren.pusing.equinan@gmail.com>
2025-10-05 21:48:18 +0800

    Update style of home component section

--- src/sections/home.jsx
+++ src/sections/home.jsx
@@ -1,8 +1,14 @@
 
-
 function Home() {
     return(
-        <section id="home" className="home"></section>
+        <section id="home" className="hero">
+           <div className="container hero-wrapper">
+                <p className="hero-text">Hello, My name is</p>
+                <h1 className="hero-heading">Warren Equinan</h1>
+                <p className="hero-text hero-description">A Creative <span>Front-End Web Developer.</span></p>
+                <a href="#" className="primary-btn hero-btn">READ MORE</a>
+           </div>
+        </section>
     )
 }
 
