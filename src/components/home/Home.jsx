b3951cc568f6e2b748db2ccd730f8e560dcab506
Ren <warren.pusing.equinan@gmail.com>
2025-10-11 22:36:13 +0800

    Update hero background

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
                 <p className="hero-text">Hello, My name is</p>
                 <h1 className="hero-heading">Warren Equinan</h1>
                 <p className="hero-text hero-description">A Creative <span>Front-End Web Developer.</span></p>
-                <a href="#" className="primary-btn hero-btn">READ MORE</a>
+                <a href="#about" className="primary-btn hero-btn">READ MORE</a>
            </div>
         </section>
     )
